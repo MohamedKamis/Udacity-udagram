f720c58add4a840bfe1708277dc46a4ca77196f2
MohamedKamis <mhmdaljwokr935@gmail.com>
2022-11-02 20:56:12 +0200

    Uplode projact 12

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -5,18 +5,18 @@ eb init udagram-api --platform node.js --region us-east-1
 
 eb use udagram-api-dev
 
-# eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME &&
-# eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD &&
-# eb setenv POSTGRES_DB=$POSTGRES_DB &&
-# eb setenv POSTGRES_HOST=$POSTGRES_HOST &&
-# eb setenv DBPORT=$DBPORT &&
-# eb setenv PORT=$PORT &&
-# eb setenv AWS_BUCKET=$AWS_BUCKET &&
-# eb setenv AWS_PROFILE=$AWS_PROFILE &&
-# eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY &&
-# eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID &&
-# eb setenv AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION &&
-# eb setenv URL=$URL &&
-# eb setenv JWT_SECRET=$JWT_SECRET  &&
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME &&
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD &&
+eb setenv POSTGRES_DB=$POSTGRES_DB &&
+eb setenv POSTGRES_HOST=$POSTGRES_HOST &&
+eb setenv DBPORT=$DBPORT &&
+eb setenv PORT=$PORT &&
+eb setenv AWS_BUCKET=$AWS_BUCKET &&
+eb setenv AWS_PROFILE=$AWS_PROFILE &&
+eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY &&
+eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID &&
+eb setenv AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION &&
+eb setenv URL=$URL &&
+eb setenv JWT_SECRET=$JWT_SECRET  &&
 
 eb deploy udagram-api-dev
