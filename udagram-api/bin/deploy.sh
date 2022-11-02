e2c7b1290cfc336c4b27e3df653f4703740a8387
MohamedKamis <mhmdaljwokr935@gmail.com>
2022-11-02 17:39:23 +0200

    Uplode projact2

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -2,17 +2,20 @@ set -x
 set -e
 
 eb init udagram-api --platform node.js --region us-east-1
-eb create --sample udagram-api-dev
+
 eb use udagram-api-dev
 
-eb setenv POSTGRES_USERNAME=postgres &&
-eb setenv POSTGRES_PASSWORD=postgres &&
-eb setenv POSTGRES_DB=udagram &&
-eb setenv POSTGRES_HOST=localhost &&
-eb setenv AWS_REGION=  &&
-eb setenv AWS_PROFILE=  &&
-eb setenv AWS_BUCKET=  &&
-eb setenv URL=http://localhost:8080  &&
-eb setenv JWT_SECRET=NEW_SECRET  
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME &&
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD &&
+eb setenv POSTGRES_DB=$POSTGRES_DB &&
+eb setenv POSTGRES_HOST=$POSTGRES_HOST &&
+eb setenv DBPORT=$DBPORT &&
+eb setenv PORT=$PORT &&
+eb setenv AWS_BUCKET=$AWS_BUCKET &&
+eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY &&
+eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID &&
+eb setenv AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION &&
+eb setenv URL=$URL &&
+eb setenv JWT_SECRET=$JWT_SECRET  &&
 
 eb deploy udagram-api-dev
