c4c8f96fef8950a089a5778234207b8383306814
MohamedKamis <mhmdaljwokr935@gmail.com>
2022-11-03 15:35:43 +0200

    finash projact6

--- udagram-frontend/dist/out-tsc/src/environments/environment.js
+++ udagram-frontend/dist/out-tsc/src/environments/environment.js
@@ -1,13 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: true,
-  appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-8ymzcjw8.us-east-1.elasticbeanstalk.com/api/v0',
+export var environment = {
+    production: false,
+    appName: 'Udagram',
+    apiHost: 'http://udagram-api-dev.eba-8ymzcjw8.us-east-1.elasticbeanstalk.com/api/v0/'
 };
-          // http://udagram-api-dev.eba-6ef5t3j8.us-east-1.elasticbeanstalk.com/api/v0/feed: 0 Unknown Error
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -16,3 +14,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+//# sourceMappingURL=environment.js.map
