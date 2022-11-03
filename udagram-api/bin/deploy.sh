a9bea25552587f898c7525e0c99d3a13062884bf
MohamedKamis <mhmdaljwokr935@gmail.com>
2022-11-03 14:18:10 +0200

    finash projact4

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -2,7 +2,7 @@
 # set -e
 
 eb init udagram-api --platform node.js --region us-east-1
-eb create --sample udagram-api-dev
+# eb create --sample udagram-api-dev
 eb use udagram-api-dev
 
 # eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME &&
